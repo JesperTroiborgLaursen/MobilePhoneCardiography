b43b0322ac98b8136a7f7fd6291e4ab9f2b5b972
CL <CL@LAPTOP-56LS1HE0>
2021-02-24 15:52:59 +0100

    Add project files.

---
+++ MobilePhoneCardiography/MobilePhoneCardiography/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MobilePhoneCardiography.Services;
+using MobilePhoneCardiography.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobilePhoneCardiography
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobilePhoneCardiography/MobilePhoneCardiography/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MobilePhoneCardiography.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MobilePhoneCardiography.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
