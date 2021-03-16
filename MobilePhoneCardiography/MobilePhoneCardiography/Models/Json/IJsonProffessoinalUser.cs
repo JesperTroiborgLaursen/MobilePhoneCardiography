b69088f91314e41440d9a6de4a4ae0aec449d92f
Emil Søndergaard <au603943@uni.au.dk>
2021-03-16 16:02:59 +0100

    iUser GetLoginMetode

---
+++ MobilePhoneCardiography/MobilePhoneCardiography/Models/Json/IJsonProffessoinalUser.cs
@@ -0,0 +1,10 @@
+﻿namespace MobilePhoneCardiography.Models.Json
+{
+    public interface IJsonProffessoinalUser
+    {
+        string _healthProfID { get; set; }
+        string _firstName { get; set; }
+        string _lastName { get; set; }
+        string _userPW { get; set; }
+    }
+}
