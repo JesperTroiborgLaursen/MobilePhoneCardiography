f40212eff597803b17208a4f19ac39b0849ba3dc
Mads Emil <58051332+memilnieAU@users.noreply.github.com>
2021-03-17 15:35:25 +0100

    Justering Enum ift. DTOs.Measurement

--- DTOs/Measurement.cs
+++ DTOs/Measurement.cs
@@ -3,28 +3,26 @@ using System.IO;
 
 namespace DTOs
 {
+    public enum PlacementOfDeviceEnum
+    {
+        CorDexter = 1,
+        CorSinister = 2,
+        CorInfra = 3
+    };
+
+
     public class Measurement
     {
-        //public List<SoundSamples> SoundSamplesList { get; set; } erstattes af Stream per 10.03.21
         public Stream SoundStream { get; set; }
         public DateTime StartTime { get; set; }
         public int ProbabilityProcent { get; set; }
         public int PatientID { get; set; }
         public int HealthProfessionalID { get; set; }
-        public enum PlacementOfDevice { CorDexter, CorSinister, CorInfra}
-        private PlacementOfDevice placement;
 
-        public PlacementOfDevice CurrentPlacementOfDevice
-        {
-            get { return placement;}
-            set { placement = value;}
-        }
-
+        public PlacementOfDeviceEnum PlacementEnum { get; set; }
         public Measurement(DateTime start)
         {
             StartTime = start;
         }
-
-        
     }
 }
