edcad128f88972b8398b106a7ee0b389ece7a179
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-04-06 14:50:29 +0200

    ConfirmButtonTest. Created UTLOginPAge

---
+++ UITest2/UTLoginPage.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITest2
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class UTLoginPage
+    {
+
+        IApp app;
+        Platform platform;
+
+        public UTLoginPage(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+
+
+
+
+    }
+}
