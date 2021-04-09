a3d5990980a48ac58f8aaadfb5d8a97a44530a45
Emil Søndergaard_Laptop <emilsndergaard>
2021-04-09 10:33:10 +0200

    Hjertelyde til træning af Machinelearning Gruppe: training-a-5

---
+++ BusinessLogic/BassFFT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DTOs;
+using Un4seen.Bass;
+using Un4seen.BassWasapi;
+
+namespace BusinessLogic
+{
+    public class BassFFT : IFFT
+    {
+        public Measurement Analyze(Measurement DTO)
+        {
+            
+            return DTO;
+        }
+    }
+}
