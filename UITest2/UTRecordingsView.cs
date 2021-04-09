d20680dd035a2552e542fd53fa09febdcd2b2d0f
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-04-09 09:49:33 +0200

    Created UTClasses for alle Views and their setup.

---
+++ UITest2/UTRecordingsView.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITest2
+{
+    [TestFixture(Platform.Android)]
+    public class UTRecordingsView
+    {
+        IApp app;
+        Platform platform;
+
+        public UTRecordingsView(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+            app.Tap("PrivateUserLoginButton");
+        }
+    }
+}
