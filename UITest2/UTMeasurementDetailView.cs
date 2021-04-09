f72ac5bd662c10b907f278e124affb6c880bea72
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-04-09 09:49:33 +0200

    Created UTClasses for alle Views and their setup.

---
+++ UITest2/UTMeasurementDetailView.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Xamarin.UITest;
+
+namespace UITest2
+{
+
+    [TestFixture(Platform.Android)]
+    public class UTMeasurementDetailView
+    {
+        IApp app;
+        Platform platform;
+
+        public UTMeasurementDetailView(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+    }
+}
