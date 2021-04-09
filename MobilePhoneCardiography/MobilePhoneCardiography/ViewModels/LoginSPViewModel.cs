795013296233d988844585533e2a68b9e4467df2
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-04-09 17:12:03 +0200

    Tried to impl. Different layouts for Pro and private user, but login doesnt work.

--- MobilePhoneCardiography/MobilePhoneCardiography/ViewModels/LoginSPViewModel.cs
+++ MobilePhoneCardiography/MobilePhoneCardiography/ViewModels/LoginSPViewModel.cs
@@ -29,7 +29,6 @@ namespace MobilePhoneCardiography.ViewModels
         {
             controllerDatabase = new ControllerDatabase(new CosmosDBService(EnumDatabase.Professionel, DateTime.Now));
 
-
             LoginCommand = new Command(OnLogin, ValidateLoginNotBlank);
             ForgotPWCommand = new Command(OnForgotPW);
             this.PropertyChanged +=
