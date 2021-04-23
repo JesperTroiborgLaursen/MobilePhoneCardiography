8afd2d5005b1299b3b12eded10d6394413c5e0cb
Jesper Trøiborg Laursen <au622885@uni.au.dk>
2021-04-23 14:49:55 +0200

    Impl. picker. Made placement info page.

---
+++ MobilePhoneCardiography/MobilePhoneCardiography/ViewModels/PlacementInfoViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using MvvmHelpers.Commands;
+using Xamarin.Forms;
+
+namespace MobilePhoneCardiography.ViewModels
+{
+    public class PlacementInfoViewModel : BaseViewModel
+
+    {
+
+
+        public PlacementInfoViewModel()
+        {
+            Title = "Placement of microphone";
+        }
+    }
+}
