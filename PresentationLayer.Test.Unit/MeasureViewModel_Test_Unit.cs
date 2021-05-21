28eb406b60f4f7ef8f843daad130d96ead4c1e4d
Sabrowna <60734481+Sabrowna@users.noreply.github.com>
2021-05-21 11:04:12 +0200

    MeasureViewModel and MeasureViewXamlCs testclasses added

---
+++ PresentationLayer.Test.Unit/MeasureViewModel_Test_Unit.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace PresentationLayer.Test.Unit
+{
+    public class MeasureViewModel_Test_Unit
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void HandleAnalyzeFinished_Call()
+        {
+            Assert.Pass();
+        }
+    }
+}
