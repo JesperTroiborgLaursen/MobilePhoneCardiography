67c78288c477fb92247d96a208e56a30fcbc6df5
Sabrowna <60734481+Sabrowna@users.noreply.github.com>
2021-05-21 13:00:45 +0200

    PresentationLayer uncommented

--- PresentationLayer.Test.Unit/MeasureViewModel_Test_Unit.cs
+++ PresentationLayer.Test.Unit/MeasureViewModel_Test_Unit.cs
@@ -1,25 +1,40 @@
 using NUnit.Framework;
+using MobilePhoneCardiography.ViewModels;
+using BusinessLogic;
+using NSubstitute;
+using DataAccessLayer.Services.Interface;
+using DataAccessLayer;
+using EventArgss;
+using System;
+
 
 namespace PresentationLayer.Test.Unit
 {
     public class MeasureViewModel_Test_Unit
     {
-        
+        private MeasureViewModel uut;
+        private IRecorderController fakeRecorderController;
+        private IRecorder fakeRecorder;
+        private ExtendedAudioRecorderService fakeAudioRecorderService;
 
         [SetUp]
         public void Setup()
         {
-
+            uut = new MeasureViewModel();
+            fakeRecorder = Substitute.For<IRecorder>();
+            fakeRecorderController = Substitute.For<IRecorderController>();
+            fakeAudioRecorderService = Substitute.For<ExtendedAudioRecorderService>();
         }
 
         [Test]
         public void HandleAnalyzeFinished_CallEvent_EventRaisesNewEvent()
         {
             //ARRANGE
-
+            
 
             //ACT
-            fakeRfidReader.RFIDDetectedEvent += Raise.EventWith(new RFIDDetectedEventArgs() { RFID = rfid });
+            //fakeRecorder.
+            //fakeRfidReader.RFIDDetectedEvent += Raise.EventWith(new RFIDDetectedEventArgs() { RFID = rfid });
             
             //ASSERT
             Assert.Pass();
